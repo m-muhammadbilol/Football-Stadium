d98143c21a43b44de239802e5e8672715d7375e9
Muhammadbilol <muhammadbilolmamasidiqov316@gmail.com>
2025-12-11 16:47:17 +0500

    music

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const elStadium = document.getElementById("stadium");
 const elBall = document.getElementById("ball");
+const elEffect = document.getElementById("effect")
 
 elStadium.addEventListener("click", (event) => {
+            
     const x = event.clientX 
     const y = event.clientY 
     elBall.style.left = x + "px";
     elBall.style.top = y + "px";
+    elEffect
 });
